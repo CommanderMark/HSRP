1a4e0ed01e91370be82d2755879317d23da6afb1
CommanderMark <commanderpro100@gmail.com>
2017-07-13 18:17:10 -0400

    d

---
+++ HSRP/Commands/Edit.cs
@@ -0,0 +1,11 @@
+using Discord.Commands;
+
+namespace HSRP.Commands
+{
+    // Stuff for modifying pre-existing player data.
+    [Group("edit"), RequireRegistration]
+    public class EditCommands : ModuleBase
+    {
+
+    }
+}
