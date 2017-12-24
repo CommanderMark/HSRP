d2665490b3ecc9919609d8fe9ee3ac4172c3a7cd
CommanderMark <commanderpro100@gmail.com>
2017-12-23 19:25:55 -0500

    Divided the site's quirks into columns.

--- Web/QuirkConverter/copy2clipboard.js
+++ Web/QuirkConverter/copy2clipboard.js
@@ -7,13 +7,15 @@ http://www.seabreezecomputers.com/tips/copy2clipboard.htm
 
 function tooltip(el, message)
 {
-    var scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft;
-    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-    var x = parseInt(el.getBoundingClientRect().left) + scrollLeft + 10;
-    var y = parseInt(el.getBoundingClientRect().top) + scrollTop + 10;
+    let scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft;
+    let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    let x = parseInt(el.getBoundingClientRect().left) + scrollLeft + 10;
+    let y = parseInt(el.getBoundingClientRect().top) + scrollTop + 10;
+
+    let tooltip;
     if (!document.getElementById("copy_tooltip"))
     {
-        var tooltip = document.createElement('div');
+        tooltip = document.createElement('div');
         tooltip.id = "copy_tooltip";
         tooltip.style.position = "absolute";
         tooltip.style.border = "1px solid black";
@@ -24,8 +26,9 @@ function tooltip(el, message)
     }
     else
     {
-        var tooltip = document.getElementById("copy_tooltip")
+        tooltip = document.getElementById("copy_tooltip")
     }
+
     tooltip.style.opacity = 1;
     tooltip.style.left = x + "px";
     tooltip.style.top = y + "px";
@@ -33,113 +36,70 @@ function tooltip(el, message)
     setTimeout(function() { tooltip.style.opacity = 0; }, 2000);
 }
 
-
-function paste(el)
-{
-    if (window.clipboardData) {
-        // IE
-        el.value = window.clipboardData.getData('Text');
-        el.innerHTML = window.clipboardData.getData('Text');
-    }
-    else if (window.getSelection && document.createRange) {
-        // non-IE
-        if (el.tagName.match(/textarea|input/i) && el.value.length < 1)
-            el.value = " "; // iOS needs element not to be empty to select it and pop up 'paste' button
-        else if (el.innerHTML.length < 1)
-            el.innerHTML = "&nbsp;"; // iOS needs element not to be empty to select it and pop up 'paste' button
-        var editable = el.contentEditable; // Record contentEditable status of element
-        var readOnly = el.readOnly; // Record readOnly status of element
-        el.contentEditable = true; // iOS will only select text on non-form elements if contentEditable = true;
-        el.readOnly = false; // iOS will not select in a read only form element
-        var range = document.createRange();
-        range.selectNodeContents(el);
-        var sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(range);
-        if (el.nodeName == "TEXTAREA" || el.nodeName == "INPUT")
-            el.select(); // Firefox will only select a form element with select()
-        if (el.setSelectionRange && navigator.userAgent.match(/ipad|ipod|iphone/i))
-            el.setSelectionRange(0, 999999); // iOS only selects "form" elements with SelectionRange
-        if (document.queryCommandSupported("paste"))
-        {
-            var successful = document.execCommand('Paste');
-            if (successful) tooltip(el, "Pasted.");
-            else
-            {
-                if (navigator.userAgent.match(/android/i) && navigator.userAgent.match(/chrome/i))
-                {
-                    tooltip(el, "Click blue tab then click Paste");
-
-                    if (el.tagName.match(/textarea|input/i))
-                    {
-                        el.value = " "; el.focus();
-                        el.setSelectionRange(0, 0);
-                    }
-                    else
-                        el.innerHTML = "";
-
-                }
-                else
-                    tooltip(el, "Press CTRL-V to paste");
-            }
-        }
-        else
-        {
-            if (!navigator.userAgent.match(/ipad|ipod|iphone|android|silk/i))
-                tooltip(el, "Press CTRL-V to paste");
-        }
-        el.contentEditable = editable; // Restore previous contentEditable status
-        el.readOnly = readOnly; // Restore previous readOnly status
-    }
-}
-
 function select_all_and_copy(el)
 {
     // Copy textarea, pre, div, etc.
-    if (document.body.createTextRange) {
+    if (document.body.createTextRange)
+    {
         // IE
-        var textRange = document.body.createTextRange();
+        let textRange = document.body.createTextRange();
         textRange.moveToElementText(el);
         textRange.select();
         textRange.execCommand("Copy");
         tooltip(el, "Copied!");
     }
-    else if (window.getSelection && document.createRange) {
+    else if (window.getSelection && document.createRange)
+    {
         // non-IE
-        var editable = el.contentEditable; // Record contentEditable status of element
-        var readOnly = el.readOnly; // Record readOnly status of element
+        let editable = el.contentEditable; // Record contentEditable status of element
+        let readOnly = el.readOnly; // Record readOnly status of element
         el.contentEditable = true; // iOS will only select text on non-form elements if contentEditable = true;
         el.readOnly = false; // iOS will not select in a read only form element
-        var range = document.createRange();
+
+        let range = document.createRange();
         range.selectNodeContents(el);
-        var sel = window.getSelection();
+        let sel = window.getSelection();
         sel.removeAllRanges();
-        sel.addRange(range); // Does not work for Firefox if a textarea or input
-        if (el.nodeName == "TEXTAREA" || el.nodeName == "INPUT")
-            el.select(); // Firefox will only select a form element with select()
+        sel.addRange(range);
+
+        // Does not work for Firefox if a textarea or input
+        // Firefox will only select a form element with select()
+        if (el.nodeName === "TEXTAREA" || el.nodeName === "INPUT")
+        {
+            el.select();
+        }
+
+        // iOS only selects "form" elements with SelectionRange
         if (el.setSelectionRange && navigator.userAgent.match(/ipad|ipod|iphone/i))
-            el.setSelectionRange(0, 999999); // iOS only selects "form" elements with SelectionRange
-        el.contentEditable = editable; // Restore previous contentEditable status
-        el.readOnly = readOnly; // Restore previous readOnly status
+        {
+            el.setSelectionRange(0, 999999);
+        }
+
+        // Restore previous contentEditable and readOnly states.
+        el.contentEditable = editable;
+        el.readOnly = readOnly;
+
         if (document.queryCommandSupported("copy"))
         {
-            var successful = document.execCommand('copy');
+            let successful = document.execCommand('copy');
             if (successful) tooltip(el, "Copied to clipboard.");
             else tooltip(el, "Press CTRL+C to copy");
         }
         else
         {
             if (!navigator.userAgent.match(/ipad|ipod|iphone|android|silk/i))
+            {
                 tooltip(el, "Press CTRL+C to copy");
+            }
         }
     }
 } // end function select_all_and_copy(el)
 
 function make_copy_button(el)
 {
-    //var copy_btn = document.createElement('button');
+    //let copy_btn = document.createElement('button');
     //copy_btn.type = "button";
-    var copy_btn = document.createElement('span');
+    let copy_btn = document.createElement('span');
     copy_btn.style.border = "1px solid black";
     copy_btn.style.padding = "5px";
     copy_btn.style.cursor = "pointer";
--- Web/QuirkConverter/highblood.js
+++ Web/QuirkConverter/highblood.js
@@ -1,143 +1,17 @@
-// Global var for which type of parsing (Quote, Trollian, ect.) to have the converter button output to.
-var outputTextType = 0;
+// Global let for which type of parsing (Quote, Trollian, ect.) to have the converter button output to.
+let outputTextType = 0;
 
 document.addEventListener('DOMContentLoaded', function()
 {
     document.getElementById("outputText").disabled = true;
     updateTextType();
     make_copy_button(document.getElementById("outputText"));
-});
-
-// Grand Highblood.
-function omvQuirk()
-{
-    var str = getInputText();
-
-    str = str.replace(/\b(\w)/g, function(match) { return match.toUpperCase(); });
-    str = str.replace(/(\w)\b/g, function(match) { return match.toUpperCase(); });
-
-    output(str);
-}
-
-// Guild Master.
-function guildMasterQuirk()
-{
-    var str = getInputText().toUpperCase();
-
-    str = str.replace(/\b(\w)/g, function(match) { return match.toLowerCase(); });
-    str = str.replace(/(\w)\b/g, function(match) { return match.toLowerCase(); });
-
-    output(str);
-}
-
-// Sicarius
-function sicariusQuirk()
-{
-    var str = getInputText();
-
-    var indexes = [];
-    var capitalIndexes = [];
-    // Index the 'm' and 'M's so that 'w's converted aren't counted.
-    var quirk = str.split("");
-    for (var i = 0; i < quirk.length; i++)
-    {
-        if (quirk[i] === "m")
-        {
-            indexes.push(i);
-        }
-        else if (quirk[i] === "M")
-        {
-            capitalIndexes.push(i);
-        }
-    }
-
-    str = str.replace(/w/g, "m");
-    str = str.replace(/W/g, "M");
-
-    quirk = str.split("");
-    for (var i = 0; i < quirk.length; i++)
-    {
-        if (indexes.includes(i))
-        {
-            quirk[i] = "w";
-        }
-        else if (capitalIndexes.includes(i))
-        {
-            quirk[i] = "W";
-        }
-    }
-
-    str = arrayToString(quirk);
-    str = str.replace(/a/g, "8");
-    str = str.replace(/A/g, "8");
-    str = str.replace(/b/g, "8");
-    str = str.replace(/B/g, "8");
-
-    output(str);
-}
-
-// HECTOR??? HE WAS THE RAT??
-function hectorQuirk()
-{
-    var quirk = getInputText().split("");
-    for (var i = 0; i < quirk.length; i++)
-    {
-        if (quirk[i] === "i")
-        {
-            quirk[i] = "iii";
-        }
-        else if (quirk[i] === "I")
-        {
-            quirk[i] = "III";
-        }
-        else if (quirk[i] === "!")
-        {
-            quirk[i] = "!!!";
-        }
-    }
-
-    output(arrayToString(quirk));
-}
-
-function vanderQuirk()
-{
-    var str = getInputText();
-
-    str = str.replace(/b/g, "♋");
-    str = str.replace(/B/g, "♋");
-    str = str.replace(/p/g, "♋");
-    str = str.replace(/P/g, "♋");
-
-    output(":Cancer: " + str);
-}
-
-// Sidewind
-function sideQuirk()
-{
-    output(getInputText());
-}
-
-// Iratar
-function iratarQuirk()
-{
-    output(">:3 " + getInputText() + " Ɛ:<");
-}
-
-// The Purifier
-function purifierQuirk()
-{
-    var str = getInputText();
-
-    str = str.replace(/t/g, "+");
-    str = str.replace(/T/g, "+");
-
-    output("♠️ " + str + " -_-");
-}
+}, true);
 
 // Convert Array back to a String.
 function arrayToString(text) {
-    var result = "";
-    for (var i = 0; i < text.length; i++) {
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
         result += text[i];
     }
 
@@ -175,7 +49,7 @@ function outputTextQuote(str)
 // With a code-lined (`foo`) anagram in front of it.
 function outputTextTrollian(str)
 {
-    var ana = document.getElementById("anagram").value;
+    let ana = document.getElementById("anagram").value;
     document.getElementById("outputText").innerHTML = "`" + ana + "`: " + str;
 }
 
