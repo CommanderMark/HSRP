70413fe435136ff41fb7fdefccb0f32fba83d606
CommanderMark <commanderpro100@gmail.com>
2017-12-25 17:43:31 -0500

    Added base files for future use.

---
+++ HSRP/Strife/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HSRP
+{
+    /// <summary>
+    /// A class for handing event triggers in a strife.
+    /// </summary>
+    public class Event
+    {
+    }
+}
---
+++ HSRP/Strife/Move.cs
@@ -0,0 +1,6 @@
+﻿namespace HSRP
+{
+    class Move
+    {
+    }
+}
